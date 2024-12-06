5c2a459689321a7617eaa02a52b917111ea196e5
Mayowa Pitan <mayowasolomon@gmail.com>
2024-12-07 00:24:56 +0100

    a bit of refactoring and formatting

--- bootcamp_labs/binary_search_spec.rb
+++ bootcamp_labs/binary_search_spec.rb
@@ -1,112 +1,118 @@
+# frozen_string_literal: true
+
 require_relative 'binary_search'
 
-RSpec.describe "Binary Search Tests" do
-  context "Binary Search to traverse an ordered list, effectively" do
-    
-    oneToTwenty = [].toTwenty;
-    twoToForty = [].toForty;
-    tenToOneThousand = [].toOneThousand;
-    
-    context "Populate the arrays with valid content" do
-      it "should create an array from 1 to 20, with intervals of 1" do
-        expect(oneToTwenty[0]).to eq 1
-        expect(oneToTwenty[19]).to eq 20
-        expect(oneToTwenty.length).to eq 20
-        for i in 0...oneToTwenty.size-1
-          expect(oneToTwenty[i+1] - oneToTwenty[i]).to eq 1
+RSpec.describe 'Binary Search Tests' do
+  context 'Binary Search to traverse an ordered list, effectively' do
+    one_to_twenty = [].to_twenty
+    two_to_forty = [].to_forty
+    ten_to_one_thousand = [].to_one_thousand
+
+    context 'Populate the arrays with valid content' do
+      it 'should create an array from 1 to 20, with intervals of 1' do
+        length = one_to_twenty.length
+        expect(one_to_twenty[0]).to eq 1
+        expect(one_to_twenty[19]).to eq 20
+        expect(length).to eq 20
+
+        (length - 1).times do |i|
+          expect(one_to_twenty[i + 1] - one_to_twenty[i]).to eq 1
         end
       end
-      
-      it "should create an array from 2 to 40, with intervals of 2" do
-        expect(twoToForty[0]).to eq 2
-        expect(twoToForty[19]).to eq 40
-        expect(twoToForty.length).to eq 20
-        for i in 0...twoToForty.size-1
-          expect(twoToForty[i+1] - twoToForty[i]).to eq 2
+
+      it 'should create an array from 2 to 40, with intervals of 2' do
+        length = two_to_forty.length
+        expect(two_to_forty[0]).to eq 2
+        expect(two_to_forty[19]).to eq 40
+        expect(length).to eq 20
+
+        (length - 1).times do |i|
+          expect(two_to_forty[i + 1] - two_to_forty[i]).to eq 2
         end
       end
-      
-      it "should create an array from 10 to 1000, with intervals of 10" do
-        expect(tenToOneThousand[0]).to eq 10
-        expect(tenToOneThousand[99]).to eq 1000
-        expect(tenToOneThousand.length).to eq 100
-        for i in 0...tenToOneThousand.size-1
-          expect(tenToOneThousand[i+1] - tenToOneThousand[i]).to eq 10
+
+      it 'should create an array from 10 to 1000, with intervals of 10' do
+        length = ten_to_one_thousand.length
+        expect(ten_to_one_thousand[0]).to eq 10
+        expect(ten_to_one_thousand[99]).to eq 1000
+        expect(length).to eq 100
+
+        (length - 1).times do |i|
+          expect(ten_to_one_thousand[i + 1] - ten_to_one_thousand[i]).to eq 10
         end
       end
     end
-    
-    context "Get the index of the item with an expected number of loops in array [1, 2 . . . 20]" do
-      it "should return {count: /* <= 4 */, index: 15} for 16" do
-        result  = oneToTwenty.search(16)
+
+    context 'Get the index of the item with an expected number of loops in array [1, 2 . . . 20]' do
+      it 'should return {count: /* <= 4 */, index: 15} for 16' do
+        result = one_to_twenty.search(16)
         expect(result[:count]).to be < 5
         expect(result[:index]).to eq 15
       end
-      
-      it "should return {count: 1, index: 18} for 19" do
-        result  = oneToTwenty.search(19)
+
+      it 'should return {count: 1, index: 18} for 19' do
+        result = one_to_twenty.search(19)
         expect(result[:count]).to be < 2
         expect(result[:index]).to eq 18
       end
-      
-      it "should return {count: 0, index: 19} for 20" do
-        result  = oneToTwenty.search(20)
+
+      it 'should return {count: 0, index: 19} for 20' do
+        result = one_to_twenty.search(20)
         expect(result[:count]).to eq 0
         expect(result[:index]).to eq 19
       end
-      
-      it "should return {count: < /* array length */, index: -1} for 33" do
-        result  = oneToTwenty.search(33)
+
+      it 'should return {count: < /* array length */, index: -1} for 33' do
+        result = one_to_twenty.search(33)
         expect(result[:count]).to be < result[:length]
-        expect(result[:index]).to eq -1
+        expect(result[:index]).to eq(-1)
       end
     end
-    
-    context "Get the index of the item with an expected number of loops in array [2, 4 . . . 40]" do
-      it "should return {count: /* <= 4 */, index: 15} for 16" do
-        result  = twoToForty.search(16)
+
+    context 'Get the index of the item with an expected number of loops in array [2, 4 . . . 40]' do
+      it 'should return {count: /* <= 4 */, index: 15} for 16' do
+        result = two_to_forty.search(16)
         expect(result[:count]).to be < 5
         expect(result[:index]).to eq 7
       end
-      
-      it "should return {count: 0, index: 9} for 20" do
-        result  = twoToForty.search(20)
+
+      it 'should return {count: 0, index: 9} for 20' do
+        result = two_to_forty.search(20)
         expect(result[:count]).to eq 0
         expect(result[:index]).to eq 9
       end
-      
-      it "should return {count: 0, index: 19} for 40" do
-        result  = twoToForty.search(40)
+
+      it 'should return {count: 0, index: 19} for 40' do
+        result = two_to_forty.search(40)
         expect(result[:count]).to eq 0
         expect(result[:index]).to eq 19
       end
-      
-      it "should return {count: < /* array length */, index: -1} for 33" do
-        result  = twoToForty.search(33)
+
+      it 'should return {count: < /* array length */, index: -1} for 33' do
+        result = two_to_forty.search(33)
         expect(result[:count]).to be < result[:length]
-        expect(result[:index]).to eq -1
+        expect(result[:index]).to eq(-1)
       end
     end
-    
-    context "Get the index of the item with an expected number of loops in array [10, 20 . . . 1000]" do
-      it "should return {count: /* <= 3 */, index: 3} for 40" do
-        result  = tenToOneThousand.search(40)
+
+    context 'Get the index of the item with an expected number of loops in array [10, 20 . . . 1000]' do
+      it 'should return {count: /* <= 3 */, index: 3} for 40' do
+        result = ten_to_one_thousand.search(40)
         expect(result[:count]).to be < 4
         expect(result[:index]).to eq 3
       end
-      
-      it "should return {count: /* <= 5*/, index: 87} for 800" do
-        result  = tenToOneThousand.search(880)
+
+      it 'should return {count: /* <= 5*/, index: 87} for 800' do
+        result = ten_to_one_thousand.search(880)
         expect(result[:count]).to be < 6
         expect(result[:index]).to eq 87
       end
-      
-      it "should return {count: < /* array length */, index: -1} for 10000" do
-        result  = tenToOneThousand.search(10000)
+
+      it 'should return {count: < /* array length */, index: -1} for 10000' do
+        result = ten_to_one_thousand.search(10_000)
         expect(result[:count]).to be < result[:length]
-        expect(result[:index]).to eq -1
+        expect(result[:index]).to eq(-1)
       end
     end
-    
   end
 end
--- enumerable.rb
+++ enumerable.rb
@@ -1,88 +1,92 @@
-# => This Enumerable module can be mixed in to the Ruby Array and Hash classes
-# => All methods in this Enumerable module  can take a block except #my_map_with_proc
+# All methods in this Enumerable module  can take a block except #my_map_with_proc
 # => Methods like #my_count and #my_map_with_proc_block may or may not take a block
 module Enumerable
-	def my_each 	# => This is the normal Ruby 'each' enumerable method
-		i = 0
-		while i < self.length
-			yield self[i]
-			i += 1
-		end
-	end
+  # => This is the normal Ruby 'each' enumerable method
+  def my_each
+    i = 0
+    while i < length
+      yield self[i]
+      i += 1
+    end
+  end
 
-	def my_each_with_index
-		i = 0
-		while i < self.length
-			yield self[i], i
-			i += 1
-		end
-	end
+  def my_each_with_index
+    i = 0
+    while i < length
+      yield self[i], i
+      i += 1
+    end
+  end
 
-	def my_select
-		newArr = []			# => methods that return an array naturally start by opening a new array
+  def my_select
+    new_arr = []	# => methods that return an array naturally start by opening a new array
 
-		my_each { |el| newArr << el if yield el }
+    my_each { |el| new_arr << el if yield el }
 
-		newArr
-	end
+    new_arr
+  end
 
-	def my_all? &block
-		self == my_select(&block)
-	end
+  def my_all?(&block)
+    self == my_select(&block)
+  end
 
-	def my_none? &block
-		myselect(&block) == []
-	end
+  def my_none?(&block)
+    myselect(&block).empty?
+  end
 
-	def my_count(item = nil, &block)	# => This method is rather involved
-		return length if item.nil? && !block_given?
+  # => This method is rather involved
+  def my_count(item = nil, &block)
+    return length if item.nil? && !block_given?
 
-		return my_select(&block).length if block_given?
+    return my_select(&block).length if block_given?
 
-		my_select { |el| el == item }.length
-	end
+    my_select { |el| el == item }.length
+  end
 
-	def my_map
-		new_arr = []
+  def my_map
+    new_arr = []
 
-		my_each { |el| new_arr << yield el }
+    my_each { |el| new_arr << (yield el) }
 
-		new_arr
-	end
+    new_arr
+  end
 
-	def my_inject(memo = 0)
-		my_each do |el|
-			memo = yield memo, el
-		end
+  def my_inject(memo = 0)
+    my_each do |el|
+      memo = yield memo, el
+    end
 
-		memo
-	end
+    memo
+  end
 
-	def my_map_with_proc(procedure) # => just like #my_map, just that it uses a proc instead of a block
-		new_arr = []
+  # => just like #my_map, just that it uses a proc instead of a block
+  def my_map_with_proc(procedure)
+    new_arr = []
 
-		my_each { |el| new_arr << procedure.call(el) }
+    my_each { |el| new_arr << procedure.call(el) }
 
-		new_arr
-	end
+    new_arr
+  end
 
-	def my_map_with_proc_block(procedure)
-		new_arr = []
+  def my_map_with_proc_block(procedure)
+    new_arr = []
 
-		my_each { |el| new_arr << procedure.call(el) }
+    my_each { |el| new_arr << procedure.call(el) }
 
-		return new_arr unless block_given?
+    return new_arr unless block_given?
 
-		latest_arr = []				# => It passes a block to the result of passing a proc to a method. It uses the #my_map algorithm
+    latest_arr = []	# => It passes a block to the result of passing a proc to a method. It uses the #my_map algorithm
 
-		new_arr.my_each { |el| latest_arr << yield el }
+    new_arr.my_each { |el| latest_arr << (yield el) }
 
-		latest_arr
-	end
+    latest_arr
+  end
 end
+
 class Array
-	include Enumerable
+  include Enumerable
 end
+
 class Hash
-	include Enumerable
+  include Enumerable
 end
